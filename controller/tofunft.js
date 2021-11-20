82436e3c98d0c39919ee70948f1c8351275bb5dc
Abraham <igamer56@gmail.com>
2021-11-20 20:38:27 +0300

    ef3

--- controller/tofunft.js
+++ controller/tofunft.js
@@ -4,50 +4,6 @@ const { default: axios } = require("axios");
 const { addDB, updatePriceDB } = require("./addDB");
 
 const header = {
-'Accept':'*/*',
-'Accept-Encoding':'gzip',
-'Accept-Language':'ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,zh-CN;q=0.5,zh;q=0.4',
-'Connection':'keep-alive',
-'Content-Length':'110',
-'Content-Type':'application/json; charset=utf-8',
-'Cookie':'_ga=GA1.1.1792403331.1637233387; _ga_ERYKG9YKMT=GS1.1.1637413079.3.1.1637414638.0',
-'DNT':'1',
-'Host':'tofunft.com',
-'Origin':'https://tofunft.com',
-'Referer':'https://tofunft.com/collection/kryptomon/items',
-'sec-ch-ua':'" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-'sec-ch-ua-mobile':'?0',
-'sec-ch-ua-platform':'"Windows"',
-'Sec-Fetch-Dest':'empty',
-'Sec-Fetch-Mode':'cors',
-'Sec-Fetch-Site':'same-origin',
-'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-'x-tofu-signature':'0000001637416599i+vDhJu++L1D293vsHNopQNE1+BqE8bya6R27+NIuQM='
-};
-
-let body = {
-    "filters": {
-        "attributes": {},
-        "contracts": [
-            232
-        ]
-    },
-    "offset": 20,
-    "limit": 20
-};
-async function getInfotofunft() {
-    // axios.post('https://tofunft.com/api/searchOrders', body, {headers: header}).then((res) => {
-    //       console.log(res);
-    //   }).catch(function (error) {
-    //     console.log("Show error notification getInfotofunft!");
-      
-    //     console.log(error);
-    //     return Promise.reject(error);
-    //   });;
-
-
-      fetch("https://tofunft.com/api/searchOrders", {
-  "headers": {
     "accept": "*/*",
     "accept-language": "ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,zh-CN;q=0.5,zh;q=0.4",
     "content-type": "application/json; charset=utf-8",
@@ -61,10 +17,29 @@ async function getInfotofunft() {
     "cookie": "_ga=GA1.1.1792403331.1637233387; _ga_ERYKG9YKMT=GS1.1.1637413079.3.1.1637414638.0",
     "Referer": "https://tofunft.com/collection/kryptomon/items",
     "Referrer-Policy": "strict-origin-when-cross-origin"
-  },
-  "body": "{\"filters\":{\"attributes\":{},\"contracts\":[232]},\"offset\":20,\"limit\":20}",
-  "method": "POST"
-});
+};
+
+let body = {
+    filters: {
+        attributes: {},
+        contracts: [
+            232
+        ]
+    },
+    offset: 20,
+    limit: 20
+};
+async function getInfotofunft() {
+    axios.post('https://tofunft.com/api/searchOrders', body, {headers: header}).then((res) => {
+          console.log(res);
+      }).catch(function (error) {
+        console.log("Show error notification getInfotofunft!");
+      
+        console.log(error);
+        return Promise.reject(error);
+      });;
+
+  
 
 }
 
