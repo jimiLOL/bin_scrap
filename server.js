f99d659d5f6f8db1b57e0ba2e79f102de9fd190c
Abraham <igamer56@gmail.com>
2021-11-21 01:06:41 +0300

    hh7

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ async function delDublicate() {
   }
   asw = [];
 }
-await delDublicate();
+delDublicate();
 // NftPokemon.find({attributes: {$elemMatch: {value: 0, trait_type: "Generation"}}}, {tokenId:1},(err, cal) => {
 //   if (err) console.log('Err');
 //   if (cal) {
