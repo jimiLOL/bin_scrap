7f47147bafd8c52ebd84c0403bbe03bc408e92ab
John Hopkins <cov12ssd@gmail.com>
2022-02-20 00:43:41 +0300

    vpc_network

--- proxy_list.js
+++ proxy_list.js
@@ -1,4 +1,4 @@
-const proxy = ["207.148.98.170:9999:username1:wqd2h2&sabdj2ASNdj6",
-    "45.32.23.142:9999:username:wqd2h2&sabdj2ASNdj6"]
+const proxy = ["10.25.96.5:9999:username1:wqd2h2&sabdj2ASNdj6",
+  ]
 
 module.exports = {proxy}
