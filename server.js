14e4e356b81c2eba57f2bf10173f9a6b02304bbb
Jimi_lol <igamer56@gmail.com>
2022-02-19 11:57:20 +0000

    add_proxy_list

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ const { cat } = require("./controller/cybercatController");
 const { filterCat } = require("./controller/faindeCate");
 const { buyNFT } = require("./controller/binance_mysteribox");
 const { getListMysterSell } = require("./controller/binance_addList_sell");
+const {getInfoBinNFTMysteryBox} = require("./controller/binance_mystery_box_misclick");
 const binanceMysterBoxAnons = require("./model/binanceMysterBoxAnons");
 const cookie = require("cookie");
 const binanceAdminCookies = require("./model/binanceAdminCookies");
@@ -60,6 +61,8 @@ mongoose.connect(process.env.MONGODB_URI).catch((error) => console.log(error));
 //     opensea();
 //   })
 // }
+
+getInfoBinNFTMysteryBox();
 
 let dataCron = new Date();
 console.log(dataCron);
