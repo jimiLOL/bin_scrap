c0d1aa88dc93bf281a756977963309cd4809c0da
John Hopkins <cov12ssd@gmail.com>
2022-02-20 00:18:12 +0300

    switch_proxy

--- proxy_list.js
+++ proxy_list.js
@@ -1,4 +1,4 @@
-const proxy = ["207.148.98.1709999username1wqd2h2&sabdj2ASNdj6",
-    "45.32.23.1429999usernamewqd2h2&sabdj2ASNdj6"]
+const proxy = ["207.148.98.170:9999:username1:wqd2h2&sabdj2ASNdj6",
+    "45.32.23.142:9999:username:wqd2h2&sabdj2ASNdj6"]
 
 module.exports = {proxy}
