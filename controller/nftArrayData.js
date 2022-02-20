495913f763da3642bac40785717d5c5f89e2b03f
John Hopkins <cov12ssd@gmail.com>
2022-02-21 01:54:36 +0300

    add_send_telegramm_info

---
+++ controller/nftArrayData.js
@@ -0,0 +1,11 @@
+const arrayNFTCollectionName = [
+{name: 'The Syphon', price: 2},
+{name: 'Genesis StarSharks Mystery Box', price: 300},
+{name: 'Kryptomon Breeding Celebration', price: 50},
+{name: 'Monsterra Treasury Box', price: 10},
+{name: 'Summoners Arena Genesis Chest', price: 10},
+{name: 'WIN NFT HORSE', price: 150},
+{name: 'Return of Legends S3', price: 10}
+]
+
+module.exports = {arrayNFTCollectionName}
