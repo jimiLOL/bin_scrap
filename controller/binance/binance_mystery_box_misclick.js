d733bfd8a9d7d87c72e64827ee58c8d56dfaf984
John Hopkins <cov12ssd@gmail.com>
2022-04-26 15:44:51 +0300

    fix

--- controller/binance/binance_mystery_box_misclick.js
+++ controller/binance/binance_mystery_box_misclick.js
@@ -181,12 +181,12 @@ async function init(init_header) {
 
                 await getInfoBinNFTMysteryBox(helper.proxyInit(proxyVar), i, body).then(res => {
                     breakSwitch = res;
-                    if (indexLayer == layer.length-1) {
+                    if (indexLayer == layerList.length-1) {
                         resolve({status: 'ok', name_worker: 'binance_mystery'})
                     }
                 }).catch(e => {
                     console.log(e);
-                    if (indexLayer == layer.length-1) {
+                    if (indexLayer == layerList.length-1) {
                         reject({status: 'error', name_worker: 'binance_mystery'})
                     }
                     // process.exit(1)
