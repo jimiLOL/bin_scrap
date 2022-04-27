1051feeebb4329dad099fd1854a0ee2ed517a430
John Hopkins <cov12ssd@gmail.com>
2022-04-27 15:10:40 +0300

    test

--- server.js
+++ server.js
@@ -69,12 +69,12 @@ let worker = {};
 let promiseWorker = [];
 
 
-worker.binance_mystery = new Piscina({
-  filename: path.resolve('./controller/binance', 'binance_mystery_box_misclick.js')
-});
-// worker.binance_marketplace = new Piscina({
-//   filename: path.resolve('./controller/binance', 'binance_marketplace_misclick.js')
+// worker.binance_mystery = new Piscina({
+//   filename: path.resolve('./controller/binance', 'binance_mystery_box_misclick.js')
 // });
+worker.binance_marketplace = new Piscina({
+  filename: path.resolve('./controller/binance', 'binance_marketplace_misclick.js')
+});
 // worker.binance_mysteryLastOrder = new Piscina({
 //   filename: path.resolve('./controller/binance', 'binance_mystery_box_lastorder.js')
 // });
