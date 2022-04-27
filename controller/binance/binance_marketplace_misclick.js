ba2234b2684c996fff37b6c1812e3bedce1e8d54
John Hopkins <cov12ssd@gmail.com>
2022-04-27 15:56:24 +0300

    test

--- controller/binance/binance_marketplace_misclick.js
+++ controller/binance/binance_marketplace_misclick.js
@@ -71,6 +71,7 @@ awaitArray = (val, length) => {
 
                     helper.timeout(2000).then(() => {
                         if (stackProxy[val].status == 'work') {
+                            console.log(Object.keys(stackProxy[val]));
                             stackProxy[val].integer++
                             console.log('===============================================================================  ' + stackProxy[val].integer);
 
