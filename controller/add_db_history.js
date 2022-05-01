52b512448d3d6ac8be8220655c2ab0c31f8fe7ae
John Hopkins <cov12ssd@gmail.com>
2022-05-01 20:42:51 +0300

    update

--- controller/add_db_history.js
+++ controller/add_db_history.js
@@ -24,10 +24,10 @@ async function add_history_binance_db(ele, marketpalce) {
                 let DateMax = Math.max(...date);
 
                 // total это количество данных по истории сделок, мы опиремся на него, как на один из индикаторов изменений
-                
+
                 if (call.total < ele.total || ele.setStartTime > DateMax) {
                     //
-                   
+
 
                     // let filter = ele.records.filter(x => x.setStartTime > DateMax);
                     // if (filter.length > 0) {
@@ -47,7 +47,7 @@ async function add_history_binance_db(ele, marketpalce) {
                     //             if (call) {
                     //                 console.log('Добавили данные в историю ' + ele.productId);
                     //                 resolve()
-                                    
+
                     //             } else {
                     //                 resolve()
                     //             }
@@ -61,81 +61,81 @@ async function add_history_binance_db(ele, marketpalce) {
 
                     // }
 
-                    let newData = {setStartTime:ele.setStartTime,amount:ele.amount,status:ele.status};
+                    let newData = { setStartTime: ele.setStartTime, amount: ele.amount, status: ele.status };
 
                     // status 4 -- закрытая сделка
-                   await NFT.findOneAndUpdate({productId: ele.productId, 'history.setStartTime': DateMax}, {$set:{'history.$.status':4}}).then(async () => {
-                        const req = await NFT.findOneAndUpdate({productId: ele.productId}, {$addToSet: {history: newData}}).then((callback)=> {
+                    await NFT.findOneAndUpdate({ productId: ele.productId, 'history.setStartTime': DateMax }, { $set: { 'history.$.status': 4 } }).then(async () => {
+                        const req = await NFT.findOneAndUpdate({ productId: ele.productId }, { $addToSet: { history: newData } }).then((callback) => {
                             if (callback) {
                                 console.log('Добавили данные в историю ' + ele.productId);
                                 resolve()
-                                
+
                             } else {
                                 resolve()
                             }
 
-                        }).catch(e=> {
+                        }).catch(e => {
                             if (e) {
                                 console.log(e);
                                 reject(e)
                             }
-                        }) 
+                        })
 
-                    }).catch(e=> {
+                    }).catch(e => {
                         console.log(e);
                         reject(e)
                     })
 
-                //    await NFT.findOneAndUpdate({productId: ele.productId, 'history.setStartTime': DateMax}, {$set:{'history.$.status':4}}, async (err, call) => {
-                //         if (err) {
-                //             console.log(err);
-                //             reject(err)
-                //         }
-                //         if (call) {
-                           
-                //             console.log('Обновили данные в историю ' + ele.productId);
-                //             // resolve()
-                            
-                //         } else {
-                //             resolve()
-                //         }
-                //     });
+                    //    await NFT.findOneAndUpdate({productId: ele.productId, 'history.setStartTime': DateMax}, {$set:{'history.$.status':4}}, async (err, call) => {
+                    //         if (err) {
+                    //             console.log(err);
+                    //             reject(err)
+                    //         }
+                    //         if (call) {
 
-                   
-                    
+                    //             console.log('Обновили данные в историю ' + ele.productId);
+                    //             // resolve()
+
+                    //         } else {
+                    //             resolve()
+                    //         }
+                    //     });
+
+
+
+                } else if (ele.amount != call?.history[call.history - 1]?.amount && ele.setStartTime == call?.history[call.history - 1]?.setStartTime) {
+                    await NFT.findOneAndUpdate({ productId: ele.productId, 'history.setStartTime': ele.setStartTime }, { $set: { 'history.$.amount': ele.amount, 'history.$.status': ele.status } }).then((callback) => {
+
+                        if (callback) {
+                            console.log('Обновили данные в историю ' + ele.productId);
+                            resolve()
+
+                        } else {
+                            resolve()
+                        }
+                    }).catch(e => {
+                        if (e) {
+                            console.log(e);
+                            reject(e)
+                        }
+
+                    })
+
+
                 } else {
                     resolve()
                 }
-            } else if(ele.amount != call?.history[call.history-1]?.amount && ele.setStartTime == call?.history[call.history-1]?.setStartTime) {
-            await NFT.findOneAndUpdate({productId: ele.productId, 'history.setStartTime': ele.setStartTime}, {$set:{'history.$.amount':ele.amount, 'history.$.status':ele.status}}).then((callback)=> {
-              
-                if (callback) {
-                    console.log('Обновили данные в историю ' + ele.productId);
-                    resolve()
-                    
-                } else {
-                    resolve()
-                }
-             }).catch(e=> {
-                if (e) {
-                    console.log(e);
-                    reject(e)
-                }
-
-             })
-     
-
             } else {
                 const binNFT = new NFT({
                     _id: new mongoose.Types.ObjectId(),
                     marketpalce: marketpalce,
-                    history: [{setStartTime:ele.setStartTime,amount:ele.amount,status:ele.status}],
+                    history: [{ setStartTime: ele.setStartTime, amount: ele.amount, status: ele.status }],
                     productId: ele.productId,
                     total: ele.total
 
                 });
                 binNFT.save().then((callback) => {
-                  
+
                     if (callback) {
                         console.log('Сохранили данные истории ' + ele.productId);
                         // console.log(binNFT);
@@ -144,7 +144,7 @@ async function add_history_binance_db(ele, marketpalce) {
                     } else {
                         reject()
                     }
-                }).catch(e=> {
+                }).catch(e => {
                     if (e) {
                         console.log('Не удалось сохранить данные истории');
                         console.log(e);
@@ -152,11 +152,11 @@ async function add_history_binance_db(ele, marketpalce) {
                         // process.exit(1)
                     };
                 })
-                    
+
 
             }
 
-        }).catch(e=> {
+        }).catch(e => {
             console.log(e);
             reject(e)
 
@@ -175,10 +175,10 @@ async function add_history_binance_db(ele, marketpalce) {
         //         let DateMax = Math.max(...date);
 
         //         // total это количество данных по истории сделок, мы опиремся на него, как на один из индикаторов изменений
-                
+
         //         if (call.total < ele.total || ele.setStartTime > DateMax) {
         //             //
-                   
+
 
         //             // let filter = ele.records.filter(x => x.setStartTime > DateMax);
         //             // if (filter.length > 0) {
@@ -198,7 +198,7 @@ async function add_history_binance_db(ele, marketpalce) {
         //             //             if (call) {
         //             //                 console.log('Добавили данные в историю ' + ele.productId);
         //             //                 resolve()
-                                    
+
         //             //             } else {
         //             //                 resolve()
         //             //             }
@@ -224,7 +224,7 @@ async function add_history_binance_db(ele, marketpalce) {
         //                     if (call) {
         //                         console.log('Добавили данные в историю ' + ele.productId);
         //                         resolve()
-                                
+
         //                     } else {
         //                         resolve()
         //                     }
@@ -241,17 +241,17 @@ async function add_history_binance_db(ele, marketpalce) {
         //         //             reject(err)
         //         //         }
         //         //         if (call) {
-                           
+
         //         //             console.log('Обновили данные в историю ' + ele.productId);
         //         //             // resolve()
-                            
+
         //         //         } else {
         //         //             resolve()
         //         //         }
         //         //     });
 
-                   
-                    
+
+
         //         } else {
         //             resolve()
         //         }
@@ -264,7 +264,7 @@ async function add_history_binance_db(ele, marketpalce) {
         //             if (call) {
         //                 console.log('Обновили данные в историю ' + ele.productId);
         //                 resolve()
-                        
+
         //             } else {
         //                 resolve()
         //             }
@@ -299,11 +299,11 @@ async function add_history_binance_db(ele, marketpalce) {
         //     }
         // }).clone()
 
-    
+
     })
 
 
 
 };
 
-module.exports = {add_history_binance_db}
+module.exports = { add_history_binance_db }
