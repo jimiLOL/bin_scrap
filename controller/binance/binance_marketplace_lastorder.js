0fcaafd4c47d7e2e9f660878262fc811767b987d
John Hopkins <cov12ssd@gmail.com>
2022-05-12 21:14:49 +0300

    update

--- controller/binance/binance_marketplace_lastorder.js
+++ controller/binance/binance_marketplace_lastorder.js
@@ -257,7 +257,7 @@ let stackProxy = {};
                     // console.log(Math.ceil(n));
                     let newData = new Date().getTime();
                     // console.log(`Date cycle^ ${newData - data} ms`);
-                    if (Math.ceil(n) == index) {
+                    if (index >= Math.ceil(n)) {
                         var_break = true
                     } // останавливаем итерацию
                     // // console.log(`Global cycle ${i}`);
