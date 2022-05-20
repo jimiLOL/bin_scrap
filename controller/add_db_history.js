3e1a354aa5dd018b550893a1183dfd962595e0c2
John Hopkins <cov12ssd@gmail.com>
2022-05-20 12:20:50 +0300

    update

--- controller/add_db_history.js
+++ controller/add_db_history.js
@@ -217,7 +217,7 @@ async function add_history_binance_db(ele, marketpalce) {
                 binNFT.save().then((callback) => {
 
                     if (callback) {
-                        console.log('Сохранили данные истории ' + ele.productId);
+                        // console.log('Сохранили данные истории ' + ele.productId);
                         // console.log(binNFT);
                         // process.exit(0)
                         ele = null;
