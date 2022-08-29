8d31076d30fc45eaf608b3ec8cc22db8ef70f22b
John Hopkins <cov12ssd@gmail.com>
2022-08-29 03:19:08 +0300

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    apps : [{
-      name   : "server",
-      script: "node_modules/pm2/node_modules/.bin/ts-node",
-      args:"-r tsconfig-paths/register -r ts-node/register ./server.ts",
-      watch: false,
-      ignore_watch:["proxy"],
-      env: {
-        "production": true
-    }
-    }]
+  apps : [{
+    name   : "server",
+    script: "node_modules/.bin/ts-node",
+    args:"-r tsconfig-paths/register -r ts-node/register ./server.ts",
+    watch: false,
+    exec_mode: 'cluster',
+    ignore_watch:["proxy"],
+    env: {
+      "production": true
   }
-  
+  }]
+}
